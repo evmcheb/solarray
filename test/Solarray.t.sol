450c695b17742e4cd98510500826dae60c56908d
Paul Razvan Berg <hello@paulrberg.com>
2022-12-27 11:55:28 +0200

    test: add tests for "uint256"

--- test/Solarray.t.sol
+++ test/Solarray.t.sol
@@ -15,6 +15,14 @@ contract SolarrayTest is Test {
         assertEq(uints[2], 2);
     }
 
+    function testUint256s() public {
+        uint256[] memory uints = Solarray.uint256s(0, 1, 2);
+        assertEq(uints.length, 3);
+        assertEq(uints[0], 0);
+        assertEq(uints[1], 1);
+        assertEq(uints[2], 2);
+    }
+
     function testStrings() public {
         string[] memory strings = Solarray.strings("a", "b", "c", "d");
         assertEq(strings.length, 4);
