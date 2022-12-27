934f06731d08b48a2716761e136dd8c064968cde
Paul Razvan Berg <hello@paulrberg.com>
2022-12-27 11:52:55 +0200

    fix: set pragma to ">=0.6.2 <0.9.0"
    test: fix casting in "testUint8s"
    test: rename "testUints" to "testUint8s"

--- test/Solarray.t.sol
+++ test/Solarray.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity >=0.6.2 <0.9.0;
 
 import "forge-std/Test.sol";
 import "../src/Solarray.sol";
@@ -7,12 +7,12 @@ import "../src/Solarray.sol";
 contract SolarrayTest is Test {
     function setUp() public {}
 
-    function testUints() public {
+    function testUint8s() public {
         uint8[] memory uints = Solarray.uint8s(0, 1, 2);
         assertEq(uints.length, 3);
-        assertEq(uints[0], uint8(0));
-        assertEq(uints[1], uint8(1));
-        assertEq(uints[2], uint8(2));
+        assertEq(uint256(uints[0]), 0);
+        assertEq(uint256(uints[1]), 1);
+        assertEq(uint256(uints[2]), 2);
     }
 
     function testStrings() public {
