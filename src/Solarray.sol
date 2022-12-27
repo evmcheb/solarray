63815fb9a8432259f580e570891e0c067253dc94
Paul Razvan Berg <hello@paulrberg.com>
2022-12-27 11:24:26 +0200

    chore: rewrite me

--- src/Solarray.sol
+++ src/Solarray.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
 
 library Solarray {
     
--- test/Solarray.t.sol
+++ test/Solarray.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/Test.sol";
 import "../src/Solarray.sol";
@@ -10,9 +10,9 @@ contract SolarrayTest is Test {
     function testUints() public {
         uint8[] memory uints = Solarray.uint8s(0, 1, 2);
         assertEq(uints.length, 3);
-        assertEq(uints[0], 0);
-        assertEq(uints[1], 1);
-        assertEq(uints[2], 2);
+        assertEq(uints[0], uint8(0));
+        assertEq(uints[1], uint8(1));
+        assertEq(uints[2], uint8(2));
     }
 
     function testStrings() public {
